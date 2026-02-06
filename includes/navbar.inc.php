04a81a00597af472102f468e36fb2ce2f1864925
Chisae <chisae0123@gmail.com>
2026-02-06 11:48:12 +0700

    update base url

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -21,20 +21,20 @@
                     <ul class="dropdown-menu">
                         <?php if (empty($user)) { ?>
                             <li><a class="dropdown-item"
-                                    href="/g19bcsy3c/?page=login">Login</a></li>
+                                    href="<?php echo $baseUrl ?>?page=login">Login</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
                             <li><a class="dropdown-item"
-                                    href="/g19bcsy3c/?page=register">Register</a></li>
+                                    href="<?php echo $baseUrl ?>?page=register">Register</a></li>
                         <?php } else { ?>
                             <li><a class="dropdown-item"
-                                    href="/g19bcsy3c/?page=profile">Profile</a></li>
+                                    href="<?php echo $baseUrl ?>?page=profile">Profile</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
                             <li><a class="dropdown-item"
-                                    href="/g19bcsy3c/?page=logout">Logout</a></li>
+                                    href="<?php echo $baseUrl ?>?page=logout">Logout</a></li>
                         <?php } ?>
                     </ul>
                 </li>
