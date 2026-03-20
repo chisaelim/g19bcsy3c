32b7ba0a8eac0872c9e518534ac46da027f04624
chisaelim <chisae0123@gmail.com>
2026-03-20 11:41:26 +0700

    update and delete user

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ $user = loggedInUser();
 $isAdmin = isAdmin();
 include './includes/header.inc.php';
 include './includes/navbar.inc.php';
-$available_pages = ['login', 'register', 'logout', 'dashboard', 'profile', 'user/list', 'user/create'];
+
 $logged_in_pages = ['dashboard', 'profile'];
 $non_logged_in_pages = ['login', 'register'];
-$admin_pages = ['user/list', 'user/create'];
+$admin_pages = ['user/list', 'user/create', 'user/update', 'user/delete'];
+$available_pages = [ // array destructuring // ... spread operator
+    'logout',
+    ...$non_logged_in_pages,
+    ...$logged_in_pages,
+    ...$admin_pages
+];
 
 $page = '';
 if (isset($_GET['page'])) {
@@ -20,7 +26,7 @@ if (in_array($page, $non_logged_in_pages) && !empty($user)) {
     header('Location: ./?page=dashboard');
 }
 if (in_array($page, $available_pages)) {
-    if(in_array($page, $admin_pages) && !$isAdmin) {
+    if (in_array($page, $admin_pages) && !$isAdmin) {
         header('Location: ./?page=dashboard');
     }
     include './pages/' . $page . '.php';
